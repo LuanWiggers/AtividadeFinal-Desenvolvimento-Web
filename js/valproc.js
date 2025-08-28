3a412d5ef464fe909ce49dfd99a6a7bef73f0e5d
LuanWiggers <luanwiggers.wiggers@gmail.com>
2025-08-27 21:50:51 -0300

    Criando as outras páginas HTML

--- js/valproc.js
+++ js/valproc.js
@@ -1,13 +1,14 @@
 function valproc(){
-// Dropdown abre/fecha ao clicar no link principal
-document.querySelectorAll(".dropdown > a").forEach(link => {
-    link.addEventListener("click", function(e) {
-        // se estiver em tela pequena, impede pular de seção
-        if (window.innerWidth <= 999) {
-            e.preventDefault();
-            const submenu = this.nextElementSibling;
-            submenu.classList.toggle("active");
-        }
+    // Dropdown abre/fecha ao clicar no link principal
+    document.querySelectorAll(".dropdown > a").forEach(link => {
+        link.addEventListener("click", function(e) {
+            // se estiver em tela pequena, impede pular de seção
+            if (window.innerWidth <= 999) {
+                e.preventDefault();
+                const submenu = this.nextElementSibling;
+                submenu.classList.toggle("active");
+            }
+        });
     });
-});
-}
+    }
+    
